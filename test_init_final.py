078bffd44d7f6d5fc44292269da86374afe11659
cool85runner <71359396+cool85runner@users.noreply.github.com>
2021-03-08 22:19:53 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3773,7 +3773,7 @@ class mainCog(commands.Cog):
 
 		embed.title = f"🎉 럭키박스! 결과발표! 🎉"
 		embed.description = ""
-	        embed.add_field(name = f"😍 당첨 ({num_cong}명)", value =  f"{', '.join(result_users)}")
+		embed.add_field(name = f"😍 당첨 ({num_cong}명)", value =  f"{', '.join(result_users)}")
 		if len(lose_user) != 0:
 		embed.add_field(name = f"😭 낙첨 ({len(lose_user)}명)", value =  f"{', '.join(lose_user)}")
 		embed.add_field(name = f"👥 참가자 ({len(user_name_list)}명)", value =  f"{', '.join(user_name_list)}", inline=False)
