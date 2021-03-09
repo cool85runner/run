64f16feb7601c66786a85cdb58264c1554b8f2d9
cool85runner <71359396+cool85runner@users.noreply.github.com>
2021-03-09 21:11:46 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3626,7 +3626,7 @@ class mainCog(commands.Cog):
 		del participant_users_by_first[del_index]
 
 		user_name_list_by_first : list = []
-		if participant_users_by_first.count > 0 :
+		if len(participant_users_by_first) > 0 :
 			for user in participant_users_by_first:
 				user_name_list_by_first.append(user.mention)
 
@@ -3649,7 +3649,7 @@ class mainCog(commands.Cog):
 		del participant_users_by_second[del_index]
 
 		user_name_list_by_second : list = []
-		if participant_users_by_second.count > 0 :
+		if len(participant_users_by_second) > 0 :
 			for user in participant_users_by_second:
 				user_name_list_by_second.append(user.mention)
 
@@ -3672,7 +3672,7 @@ class mainCog(commands.Cog):
 		del participant_users_by_stheyoungest[del_index]
 
 		user_name_list_by_stheyoungest : list = []
-		if participant_users_by_stheyoungest.count > 0 :
+		if len(participant_users_by_stheyoungest) > 0 :
 			for user in participant_users_by_stheyoungest:
 				user_name_list_by_stheyoungest.append(user.mention)
 
